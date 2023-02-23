8d11375e2223d66861191338e03089f177201d11
Camethster <camethster101@hotmail.com>
2023-02-23 10:07:45 -0700

    Smite Project starting

---
+++ Smite/smite.py
@@ -0,0 +1,10 @@
+# %%
+import pandas as pd
+import numpy as np
+import sklearn as sk
+import Automunge as am
+import altair as alt
+from sklearn import feature_selection
+from imblearn.over_sampling import RandomOverSampler
+from sklearn import svm
+import pickle
