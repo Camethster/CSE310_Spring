2fa71bc01c6daf956fd75cef49d5b47ba46f05b8
Camethster <camethster101@hotmail.com>
2023-08-14 13:36:33 -0600

    Different Dimension to the data

--- Smite/jsonParse.py
+++ Smite/jsonParse.py
@@ -7,7 +7,7 @@ import requests as rq
 import datetime
 import hashlib
 import json
-stats = ["Health","AttackSpeed","HP5","MP5","Mana","PhysicalProtection","PhysicalPower","MagicProtection","MagicalPower","Speed"]
+stats = ["Health","AttackSpeed","HP5","MP5","Mana","PhysicalProtection","PhysicalPower","MagicProtection","MagicalPower"]
 # %%""
 file = open(r"C:\Users\Camet\Portfolio\Smite\getgods.json")
 gods = json.load(file)
@@ -26,16 +26,38 @@ for atb in stats:
         healthLevel = y[atb+"PerLevel"]
         for x in range(1,20):
             level = str(x+1)
-            y["Level"+level+atb] = prevLevel + healthLevel*x
-            prevLevel = y["Level"+level+atb]
+            y[atb] = prevLevel + healthLevel*x
+            prevLevel = y[atb]
 for y in gods:
     for x in range(7,20):
         level = str(x+1)
-        print("Level"+level+"Speed")
-        print(y["Level"+level+"Speed"])
-        y.pop("Level"+level+"Speed")
+        print("Speed")
+        print(y["Speed"])
+        y.pop("Speed")
 
 # %%
 with open("parseGods.json", "w") as fp:
     json.dump(gods,fp) 
+# %%
+df = pd.DataFrame()
+df["name"] = pd.Series(str)
+df["role"] = pd.Series(str)
+df["level"] = pd.Series(int)
+for x in stats:
+    df[x] = pd.Series()
+
+#%%
+for y in gods:
+    row = pd.DataFrame.from_dict({"name" : [y["Name"]], "role" : y["Roles"], "level" : int(1), "Health" : [y["Health"]],"AttackSpeed" : [y["AttackSpeed"]],"HP5" : [y["HP5"]],"MP5" : [y["MP5"]],"Mana" : [y["Mana"]],"PhysicalProtection" : [y["PhysicalProtection"]],"PhysicalPower" : [y["PhysicalPower"]],"MagicProtection" : [y["MagicProtection"]],"MagicalPower" : [y["MagicalPower"]]},orient="columns")
+    df = pd.concat([df,row],ignore_index=True)
+    for x in range(1,20):
+            level = str(1+x)
+            row = pd.DataFrame.from_dict({"name" : [y["Name"]], "role" : y["Roles"], "level" : level, "Health" : [y["Level"+level+"Health"]],"AttackSpeed" : [y["Level"+level+"AttackSpeed"]],"HP5" : [y["Level"+level+"HP5"]],"MP5" : [y["Level"+level+"MP5"]],"Mana" : [y["Level"+level+"Mana"]],"PhysicalProtection" : [y["Level"+level+"PhysicalProtection"]],"PhysicalPower" : [y["Level"+level+"PhysicalPower"]],"MagicProtection" : [y["Level"+level+"MagicProtection"]],"MagicalPower" : [y["Level"+level+"MagicalPower"]]},orient="columns")
+            df = pd.concat([df,row],ignore_index=True)
+
+
+        
+
+# %%
+df.drop(index=0).to_csv("baseStats.csv",index=False)
 # %%
